0cda97b5d10eb84ae064d367703ab6e77f88fb77
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2024-08-30 12:06:03 +0900

    :tada: Plasmoに置き換え

--- src/contents/hello.tsx
+++ src/contents/hello.tsx
@@ -1,7 +1,5 @@
-import cssText from "data-text:~style.css"
+import cssText from "data-text:@/style.css"
 import type { PlasmoCSConfig } from "plasmo"
-
-import { CountButton } from "~features/count-button"
 
 export const config: PlasmoCSConfig = {
   matches: ["https://www.plasmo.com/*"]
@@ -15,8 +13,8 @@ export const getStyle = () => {
 
 const PlasmoOverlay = () => {
   return (
-    <div className="plasmo-z-50 plasmo-flex plasmo-fixed plasmo-top-32 plasmo-right-8">
-      <CountButton />
+    <div className="z-50 flex fixed top-32 right-8">
+      <p>HELLO!!</p>
     </div>
   )
 }
---
+++ src/popup/index.tsx
@@ -0,0 +1,14 @@
+import IconImage from "data-base64:@/assets/icon.png"
+
+import "@/style.css"
+
+function IndexPopup() {
+  return (
+    <div className="flex flex-col items-center justify-center h-[80px] w-[400px]">
+      <img className="w-[40px] h-[40px]" src={IconImage} alt="icon" />
+      <h1 className="text-xl">AI Securetary Zundamon!!</h1>
+    </div>
+  )
+}
+
+export default IndexPopup
