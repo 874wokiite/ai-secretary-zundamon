4419e975aea4d98f8099a36179e8821fd616b287
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2024-08-31 01:51:34 +0900

    :bento: ずんだもんが喋れるようにした

--- src/contents/zundamon-image.tsx
+++ src/contents/zundamon-image.tsx
@@ -1,20 +1,44 @@
 import { AspectRatio } from "@/components/ui/aspect-ratio"
-import ZundaImage from "data-base64:@/assets/zundamon-1.png"
+import ZundamonClickedImage from "data-base64:@/assets/images/zundamon-clicked.png"
+import ZundamonNormalImage from "data-base64:@/assets/images/zundamon-normal.png"
 import cssText from "data-text:@/styles/global.css"
+import { useEffect, useState } from "react"
 
 export const getStyle = () => {
   const style = document.createElement("style")
   style.textContent = cssText
+
   return style
 }
 
 const ZundamonImage = () => {
+  const [imageSrc, setImageSrc] = useState(ZundamonNormalImage)
+  var sound = new Audio(
+    chrome.runtime.getURL("assets/sounds/zundamon-greet.wav")
+  )
+
+  useEffect(() => {
+    if (imageSrc === ZundamonClickedImage) {
+      const timer = setTimeout(() => {
+        setImageSrc(ZundamonNormalImage)
+      }, 2000)
+      return () => clearTimeout(timer)
+    }
+  }, [imageSrc])
+
+  const clickEventHandler = () => {
+    setImageSrc(ZundamonClickedImage)
+    sound.play()
+  }
+
   return (
-    <div className="z-50 flex fixed bottom-[-160px] right-[-40px] w-[360px]">
-      <AspectRatio ratio={541 / 825}>
+    <div
+      className="z-50 fixed bottom-[-160px] right-[-40px] w-[360px] hover:cursor-pointer"
+      onClick={clickEventHandler}>
+      <AspectRatio ratio={2 / 3}>
         <img
           className="object-cover select-none pointer-events-none"
-          src={ZundaImage}
+          src={imageSrc}
           alt="ずんだもんの画像"
         />
       </AspectRatio>
