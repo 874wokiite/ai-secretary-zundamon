0110b3ecfaef6f878faaadac4192f6869b7153b3
tosuri13 <85057870+tosuri13@users.noreply.github.com>
2024-09-05 20:19:45 +0900

    :recycle: ContentUIをindex.tsxとして扱うように変更

---
+++ src/contents/index.tsx
@@ -0,0 +1,11 @@
+import { ZundamonImage } from "@/components/ZundamonImage";
+
+function ContentUI() {
+  return (
+    <div className="z-50 fixed bottom-[-160px] right-[-40px]">
+      <ZundamonImage />
+    </div>
+  );
+}
+
+export default ContentUI;
